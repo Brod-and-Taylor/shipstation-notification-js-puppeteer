36f2524d53a084e8deef09e89c017b6643a0368c
Warren Taylor <warren297@yahoo.com>
2022-01-24 10:25:03 -0800

    Fix Count=0 Error
    Bot was not sending notifications because it was resetting count to 0

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ var notification = null;
 browser = puppeteer
 	.launch({
 		headless: true,
-		args: ['--no-sandbox']
-	 })
+		args: ["--no-sandbox"],
+	})
 
 	.then(async (browser) => {
 		page = await browser.newPage();
@@ -64,7 +64,7 @@ browser = puppeteer
 			await page.click(
 				"#app-root > div > div > div.main-content-29r3x8m > div.edit-grid-wrap-PcFvJiX > div > div > div > div.dropdowns-wrapper-4wMCKxf > div:nth-child(4) > div > button"
 			);
-			await page.waitForTimeout(500);
+			await page.waitForTimeout(3000);
 			//"Failed"
 			await page.waitForSelector(
 				"#dropdown-wrapper-container > div > div > div > form > div > div > div.form-NMkWlYE > div > div > div:nth-child(3)"
@@ -72,50 +72,42 @@ browser = puppeteer
 			await page.click(
 				"#dropdown-wrapper-container > div > div > div > form > div > div > div.form-NMkWlYE > div > div > div:nth-child(3)"
 			);
-			await page.waitForTimeout(500);
+			await page.waitForTimeout(5000);
 			//"Apply"
 			await page.waitForSelector("#filter-form-apply");
 			await page.click("#filter-form-apply");
-			await page.waitForTimeout(5000);
+			await page.waitForTimeout(8000);
+
+			count = 0;
 
 			//click up to 10 checkboxes
 			for (let i = 1; i <= 10; i++) {
-				count = 0;
-				try {
-					//find order number
-					ordernumberID =
-						"#app-root > div > div > div.main-content-29r3x8m > div.grid-content-24RrEJI > div > div.grid-3F4ZAVB > div > div > div > div:nth-child(2) > div.non-pinned-columns-2EjEvkq > div > div:nth-child(" +
-						i +
-						") > div:nth-child(4) > button";
-					await page.waitForSelector(ordernumberID);
-					element = await page.$(ordernumberID);
-					ordernumber = await element.evaluate(
-						(el) => el.textContent
-					);
+				//find order number
+				ordernumberID =
+					"#app-root > div > div > div.main-content-29r3x8m > div.grid-content-24RrEJI > div > div.grid-3F4ZAVB > div > div > div > div:nth-child(2) > div.non-pinned-columns-2EjEvkq > div > div:nth-child(" +
+					i +
+					") > div:nth-child(4) > button";
+				await page.waitForSelector(ordernumberID);
+				element = await page.$(ordernumberID);
+				ordernumber = await element.evaluate((el) => el.textContent);
 
-					//find notification status
-					notificationID =
-						"#app-root > div > div > div.main-content-29r3x8m > div.grid-content-24RrEJI > div > div.grid-3F4ZAVB > div > div > div > div:nth-child(2) > div.non-pinned-columns-2EjEvkq > div > div:nth-child(" +
-						i +
-						") > div:nth-child(14) > div > div.message-1rzC2PB";
-					element = await page.$(notificationID);
-					notification = await element.evaluate(
-						(el) => el.textContent
-					);
+				//find notification status
+				notificationID =
+					"#app-root > div > div > div.main-content-29r3x8m > div.grid-content-24RrEJI > div > div.grid-3F4ZAVB > div > div > div > div:nth-child(2) > div.non-pinned-columns-2EjEvkq > div > div:nth-child(" +
+					i +
+					") > div:nth-child(14) > div > div.message-1rzC2PB";
+				element = await page.$(notificationID);
+				notification = await element.evaluate((el) => el.textContent);
 
-					if (notification == "Failed" && ordernumber.length > 16) {
-						checkboxID =
-							"#app-root > div > div > div.main-content-29r3x8m > div.grid-content-24RrEJI > div > div.grid-3F4ZAVB > div > div > div > div:nth-child(2) > div.pinned-columns-GW47HWR > div > div:nth-child(" +
-							i +
-							") > div > div";
-						await page.waitForSelector(checkboxID);
-						await page.click(checkboxID);
-						count += 1;
-						console.log(ordernumber);
-					}
-				} catch {
-					console.log("No more Failed Notifications");
-					break;
+				if (notification == "Failed" && ordernumber.length > 16) {
+					checkboxID =
+						"#app-root > div > div > div.main-content-29r3x8m > div.grid-content-24RrEJI > div > div.grid-3F4ZAVB > div > div > div > div:nth-child(2) > div.pinned-columns-GW47HWR > div > div:nth-child(" +
+						i +
+						") > div > div";
+					await page.waitForSelector(checkboxID);
+					await page.click(checkboxID);
+					count += 1;
+					console.log(ordernumber);
 				}
 			}
 			// if any checkboxes, press send notifications
@@ -135,7 +127,7 @@ browser = puppeteer
 		console.log("Program Ended");
 
 		// wait 1 second
-		await page.waitForTimeout(1000);
+		await page.waitForTimeout(2000);
 		// close the browser
 		await browser.close();
 	})
